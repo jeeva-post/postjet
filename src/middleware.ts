c9e7363e5976888292dbc604a98bf4577f6409bf
jeeva-post <jeemart2026@gmail.com>
2026-04-21 00:46:37 +0530

    feat: added missing middleware for clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,12 @@
-export { default } from "next-auth/middleware";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export const config = { 
-  matcher: ["/dashboard/:path*"] // డ్యాష్‌బోర్డ్ లోపల ఏ పేజీకైనా లాగిన్ తప్పనిసరి
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
